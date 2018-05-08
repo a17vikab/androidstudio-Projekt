87f9d30a6d9215d23ab2fc999d883188247756d0
a17vikab <a17vikab@student.his.se>
2018-05-08 12:04:52 +0200

    Initial commit

---
+++ app/src/main/java/com/example/a17vikab/androidstudio_projekt/Data.java
@@ -0,0 +1,70 @@
+package com.example.a17vikab.androidstudio_projekt;
+
+/**
+ * Created by a17vikab on 2018-05-08.
+ */
+
+public class Data {
+
+    public int id;
+    public String name;
+    public String company;
+    public String location;
+    public String category;
+    public int size;
+    public int cost;
+    public String auxdata;
+
+    public Data(int id, String name, String company, String location, String category, int size, int cost, String auxdata){
+        this.id = id;
+        this.name = name;
+        this.company = company;
+        this.location = location;
+        this.category = category;
+        this.size = size;
+        this.cost = cost;
+        this.auxdata = auxdata;
+    }
+
+    public int getID(){
+        return id;
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    public String getCompany(){
+        return company;
+    }
+
+    public String getLocation(){
+        return location;
+    }
+
+    public String getCategory(){
+        return category;
+    }
+
+    public int getSize(){
+        return size;
+    }
+
+    public int getCost(){
+        return cost;
+    }
+
+    public String getAuxdata(){
+        return auxdata;
+    }
+
+    public String toastText(){
+        return name + "(" + size + ") is made by " + company + " and costs " + cost +"kr at " + location + ". '" + auxdata + "'.";
+    }
+
+    @Override
+    public String toString() {
+        return name;
+    }
+
+}
---
+++ app/src/main/java/com/example/a17vikab/androidstudio_projekt/MainActivity.java
@@ -0,0 +1,180 @@
+package com.example.a17vikab.androidstudio_projekt;
+
+import android.os.AsyncTask;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.ArrayList;
+
+import static android.R.attr.id;
+
+public class MainActivity extends AppCompatActivity {
+    ArrayList<Data> dataList = new ArrayList<>();
+    ListView myListView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        FetchData fetchdata = new FetchData();
+        fetchdata.execute();
+
+        myListView = (ListView)findViewById(R.id.listview);
+
+        myListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int position, long l) {
+
+                Toast toast = Toast.makeText(getApplication(), dataList.get(position).toastText(), Toast.LENGTH_LONG);
+                toast.show();
+            }
+        });
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu){
+        getMenuInflater().inflate(R.menu.menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item){
+        int id = item.getItemId();
+
+        if(id == R.id.action_refresh)
+        {
+            dataList.clear();
+            new FetchData().execute();
+            Toast refreshed = Toast.makeText(this, "List have been refreshed", Toast.LENGTH_SHORT);
+            refreshed.show();
+
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    private class FetchData extends AsyncTask<Void,Void,String> {
+        @Override
+        protected String doInBackground(Void... params) {
+            // These two variables need to be declared outside the try/catch
+            // so that they can be closed in the finally block.
+            HttpURLConnection urlConnection = null;
+            BufferedReader reader = null;
+
+            // Will contain the raw JSON response as a Java string.
+            String jsonStr = null;
+
+            try {
+                // Construct the URL for the Internet service
+                URL url = new URL("https://wwwlab.iit.his.se/a17vikab/Programmering_av_webbapplikationer/projekt.json");
+
+                // Create the request to the PHP-service, and open the connection
+                urlConnection = (HttpURLConnection) url.openConnection();
+                urlConnection.setRequestMethod("GET");
+                urlConnection.connect();
+
+                // Read the input stream into a String
+                InputStream inputStream = urlConnection.getInputStream();
+                StringBuffer buffer = new StringBuffer();
+                if (inputStream == null) {
+                    // Nothing to do.
+                    return null;
+                }
+                reader = new BufferedReader(new InputStreamReader(inputStream));
+
+                String line;
+                while ((line = reader.readLine()) != null) {
+                    // Since it's JSON, adding a newline isn't necessary (it won't affect parsing)
+                    // But it does make debugging a *lot* easier if you print out the completed
+                    // buffer for debugging.
+                    buffer.append(line + "\n");
+                }
+
+                if (buffer.length() == 0) {
+                    // Stream was empty.  No point in parsing.
+                    return null;
+                }
+                jsonStr = buffer.toString();
+                return jsonStr;
+            } catch (IOException e) {
+                Log.e("PlaceholderFragment", "Error ", e);
+                // If the code didn't successfully get the weather data, there's no point in
+                // attempting to parse it.
+                return null;
+            } finally{
+                if (urlConnection != null) {
+                    urlConnection.disconnect();
+                }
+                if (reader != null) {
+                    try {
+                        reader.close();
+                    } catch (final IOException e) {
+                        Log.e("Network error", "Error closing stream", e);
+                    }
+                }
+            }
+        }
+        @Override
+        protected void onPostExecute(String o) {
+            super.onPostExecute(o);
+            try {
+
+                JSONArray Datalist = new JSONArray(o);
+
+                for(int i = 0; i < Datalist.length();i++) {
+                    JSONObject data = Datalist.getJSONObject(i);
+
+                    String name = data.getString("name");
+                    String company = data.getString("company");
+                    String location = data.getString("location");
+                    String category = data.getString("category");
+                    int size = data.getInt("size");
+                    int cost = data.getInt("cost");
+                    String auxdata = data.getString("auxdata");
+
+
+                    Data m = new Data(id, name, company, location, category, size, cost, auxdata);
+
+                    dataList.add(m);
+
+
+                }
+
+            } catch (JSONException e) {
+                Log.e("brom","E:"+e.getMessage());
+            }
+
+            ArrayAdapter adapter = new ArrayAdapter(getApplicationContext(), R.layout.listview,
+                    R.id.itemtextview, dataList);
+            myListView = (ListView)findViewById(R.id.listview);
+            myListView.setAdapter(adapter);
+            // This code executes after we have received our data. The String object o holds
+            // the un-parsed JSON string or is null if we had an IOException during the fetch.
+
+            // Implement a parsing code that loops through the entire JSON and creates objects
+            // of our newly created Mountain class.
+
+        }
+    }
+}
